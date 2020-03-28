b276d9415a9c3601904463449dc41c8447fcbb23
Barış Kuseyri <bariskuseyri93@gmail.com>
2020-03-28 03:06:28 +0300

    Complete phasor diagram for 27-24slot/22pole EM
    Phasor diagrams are completed. From phasors of one phase, distribution factor is obtained correctly.

--- MATLAB/Q2_phasorDiagram.m
+++ MATLAB/Q2_phasorDiagram.m
@@ -41,82 +41,84 @@ for i = 1:6
     sector.FaceAlpha = 0.2;
 end
 
-% 
-% for i = 1:27
-%     x = [0 cos(ae*(i-1))];
-%     y = [0 sin(ae*(i-1))];
-%     v(i+1)=line(x,y,'Color','k','LineStyle','--');
-%     txt = strcat(num2str(i),' \rightarrow');
-%     text(1.1*cos(ae*(i-1)),1.1*sin(ae*(i-1)),txt,'HorizontalAlignment','right')
-% end
 
-
-for i = 1 : Q
-    if i == 1
-        x = [0 cos(ae*(i-1))];
-        y = [0 sin(ae*(i-1))];
-        line(x,y,'Color','k','LineStyle','--');
-        txt1 = num2str(i);
-        text(1.1*cos(ae*(i-1)),1.1*sin(ae*(i-1)),txt1,'HorizontalAlignment','center','VerticalAlignment','middle')
-        text(1.4*cos(ae*(i-1)),1.4*sin(ae*(i-1)),'A','HorizontalAlignment','center','VerticalAlignment','middle', 'FontSize',14)
-    elseif i == 6 || i == 11 || i == 18 || i == 23
-        x = [0 cos(ae*(i-1))];
-        y = [0 sin(ae*(i-1))];
-        line(x,y,'Color','k','LineStyle','--');
-        txt = num2str(i);
-        text(1.1*cos(ae*(i-1)),1.1*sin(ae*(i-1)),txt,'HorizontalAlignment','center','VerticalAlignment','middle')
-    elseif i == 7 || i == 12 || i == 17 || i == 22
-        x = [0 -1*cos(ae*(i-1))];
-        y = [0 -1*sin(ae*(i-1))];
-        line(x,y,'Color','k','LineStyle','-');
-        txt = num2str(i+1);
-        text(-1.1*cos(ae*(i-1)),-1.1*sin(ae*(i-1)),txt,'HorizontalAlignment','center','VerticalAlignment','middle')
-      
-    elseif i == 19
-        x = [0 cos(ae*(i-1))];
-        y = [0 sin(ae*(i-1))];
-        line(x,y,'Color','k','LineStyle','--');
-        txt = num2str(i);
-        text(1.1*cos(ae*(i-1)),1.1*sin(ae*(i-1)),txt,'HorizontalAlignment','center','VerticalAlignment','middle')
-        text(1.4*cos(ae*(i-1)),1.4*sin(ae*(i-1)),'B','HorizontalAlignment','center','VerticalAlignment','middle', 'FontSize',14)
-    elseif i == 2 || i == 9 || i == 14 || i == 19 || i == 24
-        x = [0 cos(ae*(i-1))];
-        y = [0 sin(ae*(i-1))];
-        line(x,y,'Color','k','LineStyle','--');
-        txt = num2str(i);
-        text(1.1*cos(ae*(i-1)),1.1*sin(ae*(i-1)),txt,'HorizontalAlignment','center','VerticalAlignment','middle')
-    elseif i == 3 || i == 8 || i == 13 || i == 25
-        x = [0 -1*cos(ae*(i-1))];
-        y = [0 -1*sin(ae*(i-1))];
-        line(x,y,'Color','k','LineStyle','-');
-        txt = num2str(i+1);
-        text(-1.1*cos(ae*(i-1)),-1.1*sin(ae*(i-1)),txt,'HorizontalAlignment','center','VerticalAlignment','middle')
-        
-    elseif i == 10
-        x = [0 cos(ae*(i-1))];
-        y = [0 sin(ae*(i-1))];
-        line(x,y,'Color','k','LineStyle','--');
-        txt = num2str(i);
-        text(1.1*cos(ae*(i-1)),1.1*sin(ae*(i-1)),txt,'HorizontalAlignment','center','VerticalAlignment','middle')
-        text(1.4*cos(ae*(i-1)),1.4*sin(ae*(i-1)),'C','HorizontalAlignment','center','VerticalAlignment','middle', 'FontSize',14)    
-    elseif i == 5 || i == 10 || i == 15 || i == 20 || i == 27
-        x = [0 cos(ae*(i-1))];
-        y = [0 sin(ae*(i-1))];
-        line(x,y,'Color','k','LineStyle','--');
-        txt = num2str(i);
-        text(1.1*cos(ae*(i-1)),1.1*sin(ae*(i-1)),txt,'HorizontalAlignment','center','VerticalAlignment','middle')
-    elseif i == 4 || i == 16 || i == 21 || i == 26
-        x = [0 -1*cos(ae*(i-1))];
-        y = [0 -1*sin(ae*(i-1))];
-        line(x,y,'Color','k','LineStyle','-');
-        txt = num2str(i+1);
-        text(-1.1*cos(ae*(i-1)),-1.1*sin(ae*(i-1)),txt,'HorizontalAlignment','center','VerticalAlignment','middle')
-    
-    else
-        msgbox('Operation Failed');
-        
-    end   
+for i = 1:27
+    x = [0 cos(ae*(i-1))];
+    y = [0 sin(ae*(i-1))];
+    v(i+1)=line(x,y,'Color','k','LineStyle','--');
+    txt = num2str(i);
+    text(1.1*cos(ae*(i-1)),1.1*sin(ae*(i-1)),txt,'HorizontalAlignment','center','VerticalAlignment','middle')
 end
+
+
+
+% 
+% for i = 1 : Q
+%     if i == 1
+%         x = [0 cos(ae*(i-1))];
+%         y = [0 sin(ae*(i-1))];
+%         line(x,y,'Color','k','LineStyle','--');
+%         txt1 = num2str(i);
+%         text(1.1*cos(ae*(i-1)),1.1*sin(ae*(i-1)),txt1,'HorizontalAlignment','center','VerticalAlignment','middle')
+%         text(1.4*cos(ae*(i-1)),1.4*sin(ae*(i-1)),'A','HorizontalAlignment','center','VerticalAlignment','middle', 'FontSize',14)
+%     elseif i == 6 || i == 11 || i == 18 || i == 23
+%         x = [0 cos(ae*(i-1))];
+%         y = [0 sin(ae*(i-1))];
+%         line(x,y,'Color','k','LineStyle','--');
+%         txt = num2str(i);
+%         text(1.1*cos(ae*(i-1)),1.1*sin(ae*(i-1)),txt,'HorizontalAlignment','center','VerticalAlignment','middle')
+%     elseif i == 7 || i == 12 || i == 17 || i == 22
+%         x = [0 -1*cos(ae*(i-1))];
+%         y = [0 -1*sin(ae*(i-1))];
+%         line(x,y,'Color','k','LineStyle','-');
+%         txt = num2str(i+1);
+%         text(-1.1*cos(ae*(i-1)),-1.1*sin(ae*(i-1)),txt,'HorizontalAlignment','center','VerticalAlignment','middle')
+%       
+%     elseif i == 19
+%         x = [0 cos(ae*(i-1))];
+%         y = [0 sin(ae*(i-1))];
+%         line(x,y,'Color','k','LineStyle','--');
+%         txt = num2str(i);
+%         text(1.1*cos(ae*(i-1)),1.1*sin(ae*(i-1)),txt,'HorizontalAlignment','center','VerticalAlignment','middle')
+%         text(1.4*cos(ae*(i-1)),1.4*sin(ae*(i-1)),'B','HorizontalAlignment','center','VerticalAlignment','middle', 'FontSize',14)
+%     elseif i == 2 || i == 9 || i == 14 || i == 19 || i == 24
+%         x = [0 cos(ae*(i-1))];
+%         y = [0 sin(ae*(i-1))];
+%         line(x,y,'Color','k','LineStyle','--');
+%         txt = num2str(i);
+%         text(1.1*cos(ae*(i-1)),1.1*sin(ae*(i-1)),txt,'HorizontalAlignment','center','VerticalAlignment','middle')
+%     elseif i == 3 || i == 8 || i == 13 || i == 25
+%         x = [0 -1*cos(ae*(i-1))];
+%         y = [0 -1*sin(ae*(i-1))];
+%         line(x,y,'Color','k','LineStyle','-');
+%         txt = num2str(i);
+%         text(-1.1*cos(ae*(i-1)),-1.1*sin(ae*(i-1)),txt,'HorizontalAlignment','center','VerticalAlignment','middle')
+%         
+%     elseif i == 10
+%         x = [0 cos(ae*(i-1))];
+%         y = [0 sin(ae*(i-1))];
+%         line(x,y,'Color','k','LineStyle','--');
+%         txt = num2str(i);
+%         text(1.1*cos(ae*(i-1)),1.1*sin(ae*(i-1)),txt,'HorizontalAlignment','center','VerticalAlignment','middle')
+%         text(1.4*cos(ae*(i-1)),1.4*sin(ae*(i-1)),'C','HorizontalAlignment','center','VerticalAlignment','middle', 'FontSize',14)    
+%     elseif i == 5 || i == 10 || i == 15 || i == 20 || i == 27
+%         x = [0 cos(ae*(i-1))];
+%         y = [0 sin(ae*(i-1))];
+%         line(x,y,'Color','k','LineStyle','--');
+%         txt = num2str(i);
+%         text(1.1*cos(ae*(i-1)),1.1*sin(ae*(i-1)),txt,'HorizontalAlignment','center','VerticalAlignment','middle')
+%     elseif i == 4 || i == 16 || i == 21 || i == 26
+%         x = [0 -1*cos(ae*(i-1))];
+%         y = [0 -1*sin(ae*(i-1))];
+%         line(x,y,'Color','k','LineStyle','-');
+%         txt = num2str(i+1);
+%         text(-1.1*cos(ae*(i-1)),-1.1*sin(ae*(i-1)),txt,'HorizontalAlignment','center','VerticalAlignment','middle')
+%     
+%     else
+%         msgbox('Operation Failed');
+%         
+%     end   
+% end
 
 set(gca,'visible','off')
 set(gca,'xtick',[])
